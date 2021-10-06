3a536780d2e4187b6ed8f9ca24c048234432d8fe
Rohan Shetty <rshetty449@gmail.com>
2021-10-05 22:02:49 -0400

    added mirror and new car physics

---
+++ Assets/Scripts/driving.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driving : MonoBehaviour
+{
+    Rigidbody rb;
+    Transform tf;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 4f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("q"))
+        {
+            Vector3 force = transform.forward * speed;
+            Vector3 startPos = transform.position + (Quaternion.AngleAxis(-90, Vector3.up) * transform.forward * (float)(transform.localScale.magnitude));
+            startPos.y += transform.localScale.magnitude * 0.5f;
+            rb.AddForceAtPosition(force, startPos);
+            Debug.DrawLine(startPos, startPos + force * 10);
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector3 force = -transform.forward * speed;
+            Vector3 startPos = transform.position + (Quaternion.AngleAxis(-90, Vector3.up) * transform.forward * (float)(transform.localScale.magnitude));
+            startPos.y += transform.localScale.magnitude * 0.5f;
+            rb.AddForceAtPosition(force, startPos);
+            Debug.DrawLine(startPos, startPos + force * 10);
+        }
+        if (Input.GetKey("p"))
+        {
+            Vector3 force = transform.forward * speed;
+            Vector3 startPos = transform.position + (Quaternion.AngleAxis(90, Vector3.up) * transform.forward * (float)(transform.localScale.magnitude));
+            startPos.y += transform.localScale.magnitude * 0.5f;
+            rb.AddForceAtPosition(force, startPos);
+            Debug.DrawLine(startPos, startPos + force * 10);
+        }
+        if (Input.GetKey("l"))
+        {
+            Vector3 force = -transform.forward * speed;
+            Vector3 startPos = transform.position + (Quaternion.AngleAxis(90, Vector3.up) * transform.forward * (float)(transform.localScale.magnitude));
+            startPos.y += transform.localScale.magnitude * 0.5f;
+            rb.AddForceAtPosition(force, startPos);
+            Debug.DrawLine(startPos, startPos + force * 10);
+        }
+    }
+}
