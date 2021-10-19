faa71915727e0fee381e4e82ce9e6a88e57c9a46
Fred Yang <fredyangify@gmail.com>
2021-10-19 09:32:23 -0700

    box trigger that can detect parking

---
+++ Assets/Scripts/Destination.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+
+    DateTime parkStartTime;
+    TimeSpan parkTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        parkStartTime = DateTime.Now;
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.attachedRigidbody.velocity.magnitude < 0.5)
+        {
+            parkTime = DateTime.Now.Subtract(parkStartTime);
+            if (parkTime.Seconds > 5)
+            {
+                Debug.Log("The penis is parked");
+            }
+
+        }
+        else
+        {
+            parkStartTime = DateTime.Now;
+        }
+    }
+}
