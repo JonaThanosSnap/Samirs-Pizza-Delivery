f89c588f032d3bbbec6e40545593ecc0227ccba2
Kevin Gao <kevingao2003@gmail.com>
2021-10-21 19:03:55 -0400

    fix lcd

--- sketch_oct06a.ino
+++ sketch_oct06a.ino
@@ -1,15 +1,22 @@
+#include <LiquidCrystal.h> 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 #define joyX1 A0
 #define joyX2 A1
-
 float x1Read = 0;
 float x2Read = 0;
 float x1Value = 0;
 float x2Value = 0;
+
 void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
   Serial.begin(9600);
 }
  
 void loop() { 
+  //joysticks
   // put your main code here, to run repeatedly:
   x1Read = analogRead(joyX1);
   x2Read = analogRead(joyX2);
@@ -23,5 +30,8 @@ void loop() {
   Serial.print(x1Value);
   Serial.print("\t");
   Serial.println(x2Value);
-
+  
+  //lcd
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
 }
