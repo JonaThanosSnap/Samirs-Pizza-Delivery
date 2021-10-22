cb5dbd6267c2b29873eb1b91d06be3d50e0e2eee
Rohan Shetty <rshetty449@gmail.com>
2021-10-21 23:36:34 -0400

    added smooth sync

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,7 +15,7 @@ public class CameraFollow : NetworkBehaviour
     void Update()
     {
         // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, 10));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 8, 12));
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
