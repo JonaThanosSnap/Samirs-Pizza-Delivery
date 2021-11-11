1676ad47c6ec9816d89ab58714385e6b0c50b3d0
Jonathan Ge <59585116+JonaThanosSnap@users.noreply.github.com>
2021-11-11 16:27:29 -0500

    Added script to load into game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame (){
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+
+    }
+    
+}
